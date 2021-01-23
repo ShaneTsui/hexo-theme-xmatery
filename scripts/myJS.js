3fbdc47fc6e7689878226a11fcc316ed4cf03c77
ShaneTsui <shanetsui.cs@gmail.com>
2021-01-23 11:22:32 -0800

    Add table to markdown tool

--- scripts/myJS.js
+++
@@ -1,8 +0,0 @@
-function postNote(args, content) {
-    return `<div class="note ${args.join(' ')}">
-            ${hexo.render.renderSync({text: content, engine: 'markdown'}).split('\n').join('')}
-          </div>`;
-}
-
-hexo.extend.tag.register('note', postNote, {ends: true});
-hexo.extend.tag.register('subnote', postNote, {ends: true});
